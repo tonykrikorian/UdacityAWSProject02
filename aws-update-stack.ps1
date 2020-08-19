1427af3e6ac039869eb717c6dc2e89dca58ba0ce
Tony Krikorian <tony.krikorian@outlook.com>
2020-08-19 07:52:45 -0400

    Primer commit

---
+++ aws-update-stack.ps1
@@ -0,0 +1,5 @@
+$stack = $args[0]
+$template = $args[1]
+$params = $args[2]
+
+aws cloudformation update-stack --stack-name $stack --region us-west-2 --template-body file://$template --parameters file://$params
