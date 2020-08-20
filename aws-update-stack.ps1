7558c3416cc249aebeefc0ca28db276656d6df2d
Tony Krikorian <tony.krikorian@outlook.com>
2020-08-20 10:02:02 -0400

    Deploy servers

--- aws-update-stack.ps1
+++ aws-update-stack.ps1
@@ -2,4 +2,4 @@ $stack = $args[0]
 $template = $args[1]
 $params = $args[2]
 
-aws cloudformation update-stack --stack-name $stack --region us-west-2 --template-body file://$template --parameters file://$params
+aws cloudformation update-stack --stack-name $stack --region us-west-2 --template-body file://$template --parameters file://$params --capabilities CAPABILITY_IAM
